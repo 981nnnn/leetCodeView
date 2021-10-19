e320d4fd6094cab36674ccaf9d72f4477449df4d
wxb <wangxiaobao@hd123.com>
2021-10-19 11:21:01 +0800

    title:<feat>剑指 Offer 06. 从尾到头打印链表 body: 1.循环遍历链表中的值，将其放入stack栈中（先进后出），链表中头节点的数据在栈底，链表中尾部节点的数据在栈顶。 2.遍历stack就可以实现从尾到头打印链表

---
+++ src/leetcode/editor/cn/CongWeiDaoTouDaYinLianBiaoLcof.java
@@ -0,0 +1,55 @@
+package leetcode.editor.cn;
+
+//输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：head = [1,3,2]
+//输出：[2,3,1] 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 链表长度 <= 10000 
+// Related Topics 栈 递归 链表 双指针 👍 195 👎 0
+
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.util.Stack;
+
+public class CongWeiDaoTouDaYinLianBiaoLcof{
+    public static void main(String[] args) {
+        Solution solution = new CongWeiDaoTouDaYinLianBiaoLcof().new Solution();
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+//*
+// Definition for singly-linked list.
+ public class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+ }
+
+class Solution {
+    public int[] reversePrint(ListNode head) {
+        Stack<ListNode> stack = new Stack<ListNode>();
+        ListNode temp = head;
+        while (temp != null) {
+            stack.push(temp);
+            temp = temp.next;
+        }
+        int size = stack.size();
+        int[] print = new int[size];
+        for (int i = 0; i < size; i++) {
+            print[i] = stack.pop().val;
+        }
+        return print;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
