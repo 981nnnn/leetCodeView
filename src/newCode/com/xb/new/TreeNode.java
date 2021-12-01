0bb29c6673f1a661cb3e07434e540aa657dab7ba
wxb <wangxiaobao@hd123.com>
2021-12-01 19:53:31 +0800

    1

---
+++ src/newCode/com/xb/new/TreeNode.java
@@ -0,0 +1,19 @@
+/**
+ * @ClassName TreeNode
+ * @Description TODO
+ * @Author xb
+ * @Date 2021/11/30 9:44
+ * @Version 1.0
+ **/
+public class TreeNode {
+  int val = 0;
+  TreeNode left = null;
+  TreeNode right = null;
+
+  public TreeNode(int val) {
+    this.val = val;
+
+  }
+
+
+}
